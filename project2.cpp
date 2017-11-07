62ccdbf5fbd49851236b98e18a287049298e1cea
Gabriel A Contreras <gabrielc1@mail.usf.edu>
2017-11-07 18:58:57 -0500

    Changes

--- project2.cpp
+++ project2.cpp
@@ -1,73 +1,119 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <algorithm>
 #include <stack>
+#include <queue>
+#include <limits>
+#include <io.h>
+
 using namespace std;
 
-int main()
-{
-	string str, dimensions;
-	ifstream input("input.txt", fstream::in);
-	if (!input) {
-		cout << "Error opening input.txt for reading." << endl;
-		return 1;
-	}
-	ifstream output("output.txt", fstream::out);
-	if (!output) {
-		cout << "Error opening output.txt for writing." << endl;
-		return 1;
-	}
-
-	int r, c;
-	input >> r >> c;
-	int i, j;
-	double **in;
-	initializeMatrix(in, r, c);
-
-	for (i = 0; i < r; ++i) {
-		for (j = 0; j < c; ++j)
-			input >> in[i][j];
-	}
-
-	double **costMap;
-	initializeMatrix(costMap, r, c);
-	costMap = iterDPMap(in, r, c);
-	/*for (i = 0; i < c; ++i)
-	{
-		
-	}*/
-
-}
-
-inline void initializeMatrix(double** arr, int r, int c) {
-	int i;
-	arr = new double*[r];
-	for (i = 0; i < r; ++i)
-		arr[i] = new double[c];
+inline void initializeMatrix(double*** arr, int r, int c) {
+	*arr = new double*[r];
+	for (int i = 0; i < r; ++i)
+        (*arr)[i] = new double[c];
 }
 
 double** iterDPMap(double** in, int r, int c) {
-	int i, j;
+	double min;
 	double** ans;
-	initializeMatrix(ans, r, c);
+	double max = numeric_limits<double>::infinity();
+	initializeMatrix(&ans, r, c);
 
-	for (i = 0; i < r; ++i) {
-		for (j = 0; j < c; ++j) {
-			if (i == 0) {
-				ans[0][j] = min(
-					j > 0 ? 1.4 * in[0][j - 1] : INFINITY,
-					in[0][j],
-					j < c - 1 ? 1.4 * in[0][j + 1] : INFINITY);
-			}
+	for (int i = 0; i < r; ++i) {
+		for (int j = 0; j < c; ++j){
+			if (i == 0)
+				ans[0][j] =	in[0][j];
 			else
 			{
-				ans[i][j] = min(
-					j > 0 ? in[i - 1][j - 1] + 1.4 * in[i][j - 1] : INFINITY,
-					ans[i - 1][j] + in[i][j],
-					j < c - 1 ? ans[i - 1][j + 1] + 1.4 * in[i][j + 1] : INFINITY);
+                min = ans[i-1][j] + in[i][j];
+
+                if(j > 0 && ans[i-1][j-1] + 1.4 * in[i][j] < min)
+                    min = ans[i-1][j-1] + 1.4 * in[i][j];
+                if(j < c-1 && ans[i-1][j+1] + 1.4 * in[i][j] < min)
+                    min = ans[i-1][j+1] + 1.4 * in[i][j];
+
+				ans[i][j] = min;
 			}
 		}
 	}
 	return ans;
 }
+
+queue<string> fastestPaths(double** map, double** in, int r, int c) {
+	stack <string> path;
+	queue <string> ans;
+	for (int j = 0; j < c; j++){
+		int loc = j;
+		for(int i = r-1; i > 0; i--){
+			if(loc > 0 && map[i][loc] == map[i-1][loc-1] + 1.4 * in[i][loc]){
+				path.push("SE");
+				loc--;
+			} else if(loc < c-1 && map[i][loc] == map[i-1][loc+1] + 1.4 * in[i][loc]){
+				path.push("SW");
+				loc++;
+			} else
+				path.push("S");
+		}
+		ostringstream str1;
+		str1 << loc;
+		string loc_string = str1.str();
+
+		path.push(loc_string);
+		while (!path.empty()){
+			ans.push(static_cast<basic_string<char> &&>(path.top()));
+			path.pop();
+		}
+	}
+	return ans;
+}
+
+int main()
+{
+	ifstream input;
+    input.open("C:\\Users\\Gabriel\\Downloads\\input-small.txt");
+	if (!input) {
+		cout << "Error opening input.txt for reading." << endl;
+		return 1;
+	}
+
+//    ifstream output("output.txt", fstream::out);
+//    if (!output) {
+//        cout << "Error opening output.txt for writing." << endl;
+//        return 1;
+//    }
+
+	int r , c;
+	input >>  r;
+    input >> c;
+	double **in;
+	initializeMatrix(&in, r, c);
+    int x;
+
+	for (int i = 0; i < r; ++i) {
+        for (int j = 0; j < c; ++j)
+            input >> in[i][j];
+
+	}
+
+    input.close();
+
+	double **costMap;
+	initializeMatrix(&costMap, r, c);
+	costMap = iterDPMap(in, r, c);
+
+	queue <string> answer = fastestPaths(costMap, in, r, c);
+
+	for (int i = 0; i < c; ++i)
+	{
+		for(int j = 0; i < r; ++j){
+			cout << answer.front();
+			answer.pop();
+		}
+		cout << endl;
+	}
+
+}
+
